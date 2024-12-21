e201a66f5a11ec3fb4cb9119b564221d1ecef40d
Alisson Alves <105590677+aas1989@users.noreply.github.com>
2024-12-21 14:00:13 -0300

    Projeto contaBanco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Scanner conta = new Scanner(System.in);
+
+        int numero;
+        String Agencia;
+        String NomeCliente;
+        float Saldo;
+
+        System.out.println("Por favor digite o número da sua conta");
+        numero = conta.nextInt();
+    
+        System.out.println("Por favor digite o número da agência");
+        Agencia = conta.next();
+
+        conta.nextLine();
+        
+        System.out.println("Por favor digite seu nome");
+        NomeCliente = conta.nextLine();
+       
+        System.out.println("Digite o valor do seu depósito");
+        String entradaSaldo = conta.next();
+        entradaSaldo = entradaSaldo.replace(",",".");
+
+        try {
+            Saldo = Float.parseFloat(entradaSaldo);
+        } catch (NumberFormatException e) {
+            Saldo = 0.00f;
+        }
+        
+        System.out.println("Olá, " + NomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + " conta "  + numero + " e seu saldo R$" + Saldo + " já está disponivel para saque!");
     }
 }
